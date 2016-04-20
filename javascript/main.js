ead500147b211afb48f1fc9950bc3ef19a302247
agdesigncreatif <nick.tkachov@hotmail.com>
2016-04-20 18:00:52 -0400

    df
    ff

--- javascript/main.js
+++ javascript/main.js
@@ -70,25 +70,25 @@ window.onload = function () {
             open = false;
         }
     }
-    insertProducts("image2.jpg", "20", "Pair of Pink Earrings", "1", "medium", "earrings");
+    insertProducts("image2.JPG", "20", "Pair of Pink Earrings", "1", "medium", "earrings");
     insertProducts("image3.jpg", "21", "Pair of Pink Bracelets", "1", "medium", "bracelets");
-    insertProducts("image6.jpg", "22", "Pin", "1", "medium", "scarf");
+    insertProducts("image6.JPG", "22", "Pin", "1", "medium", "scarf");
     insertProducts("image1.jpg", "23", "Yellow Necklace", "1", "medium", "necklaces");
     insertProducts("image4.jpg", "24", "Turquoise Bracelet with Pattern", "1", "medium", "bracelets");
-    insertProducts("image9.jpg", "25", "Multiple Colorful Pins", "1", "medium", "scarf");
+    insertProducts("image9.JPG", "25", "Multiple Colorful Pins", "1", "medium", "scarf");
     insertProducts("image7.jpg", "26", "Two Hexagon-Shaped Pins", "1", "medium", "scarf");
-    insertProducts("image8.jpg", "27", "Red and Colorful Pins", "1", "medium", "scarf");
-    insertProducts("image10.jpg", "28", "Yellow Earrings", "1", "medium", "earrings");
-    insertProducts("image11.jpg", "29", "Pink Earrings", "1", "medium", "earrings");
-    insertProducts("image12.jpg", "30", "Orange Earrings w/ Bracelet", "1", "medium", "earrings");
-    insertProducts("image13.jpg", "31", "Blue Earrings w/ Necklace", "1", "medium", "earrings");
-    insertProducts("image14.jpg", "32", "Red Earrings w/ Necklace", "1", "medium", "earrings");
-    insertProducts("image15.jpg", "33", "Blue and Pink Bracelet", "1", "medium", "bracelets");
-    insertProducts("image16.jpg", "34", "Turquoise Bracelet", "1", "medium", "bracelets");
-    insertProducts("image17.jpg", "35", "Red and White Pin", "1", "medium", "scarf");
-    insertProducts("image18.jpg", "36", "Yellow Bracelet", "1", "medium", "bracelets");
-    insertProducts("image19.jpg", "37", "Pink Earrings w/ Bracelet", "1", "medium", "earrings");
-    insertProducts("image20.jpg", "38", "Colorful Earrings", "1", "medium", "earrings");
+    insertProducts("image8.JPG", "27", "Red and Colorful Pins", "1", "medium", "scarf");
+    insertProducts("image10.JPG", "28", "Yellow Earrings", "1", "medium", "earrings");
+    insertProducts("image11.JPG", "29", "Pink Earrings", "1", "medium", "earrings");
+    insertProducts("image12.JPG", "30", "Orange Earrings w/ Bracelet", "1", "medium", "earrings");
+    insertProducts("image13.JPG", "31", "Blue Earrings w/ Necklace", "1", "medium", "earrings");
+    insertProducts("image14.JPG", "32", "Red Earrings w/ Necklace", "1", "medium", "earrings");
+    insertProducts("image15.JPG", "33", "Blue and Pink Bracelet", "1", "medium", "bracelets");
+    insertProducts("image16.JPG", "34", "Turquoise Bracelet", "1", "medium", "bracelets");
+    insertProducts("image17.JPG", "35", "Red and White Pin", "1", "medium", "scarf");
+    insertProducts("image18.JPG", "36", "Yellow Bracelet", "1", "medium", "bracelets");
+    insertProducts("image19.JPG", "37", "Pink Earrings w/ Bracelet", "1", "medium", "earrings");
+    insertProducts("image20.JPG", "38", "Colorful Earrings", "1", "medium", "earrings");
 
     var earrings = document.getElementsByClassName("earrings")
     var necklaces = document.getElementsByClassName("necklaces")
