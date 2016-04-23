88d0f682bd599b5c4e603693ea3a7822ae90eb69
agdesigncreatif <nick.tkachov@hotmail.com>
2016-04-22 21:57:24 -0400

    added desc

--- javascript/products.js
+++ javascript/products.js
@@ -9,25 +9,25 @@
 //beads2 = BEADED CROCHET BRACELETS
 //scarf = HIJAB, SCARF & SHAWL PINS
 
-insertProducts("image2.JPG", "20", "Pair of Pink Earrings", "1", "medium", "earrings", "");
-insertProducts("image3.jpg", "21", "Pair of Pink Bracelets", "1", "medium", "bracelets", "");
-insertProducts("image6.JPG", "22", "Pin", "1", "medium", "scarf", "");
-insertProducts("image1.jpg", "23", "Yellow Necklace", "1", "medium", "necklaces", "");
-insertProducts("image4.jpg", "24", "Turquoise Bracelet with Pattern", "1", "medium", "bracelets", "");
-insertProducts("image9.JPG", "25", "Multiple Colorful Pins", "1", "medium", "scarf", "");
-insertProducts("image7.jpg", "26", "Two Hexagon-Shaped Pins", "1", "medium", "scarf", "");
-insertProducts("image8.JPG", "27", "Red and Colorful Pins", "1", "medium", "scarf", "");
-insertProducts("image10.JPG", "28", "Yellow Earrings", "1", "medium", "earrings", "");
-insertProducts("image11.JPG", "29", "Pink Earrings", "1", "medium", "earrings", "");
-insertProducts("image12.JPG", "30", "Orange Earrings w/ Bracelet", "1", "medium", "earrings", "");
-insertProducts("image13.JPG", "31", "Blue Earrings w/ Necklace", "1", "medium", "earrings", "");
-insertProducts("image14.JPG", "32", "Red Earrings w/ Necklace", "1", "medium", "earrings", "");
-insertProducts("image15.JPG", "33", "Blue and Pink Bracelet", "1", "medium", "bracelets", "");
-insertProducts("image16.JPG", "34", "Turquoise Bracelet", "1", "medium", "bracelets", "");
-insertProducts("image17.JPG", "35", "Red and White Pin", "1", "medium", "scarf", "");
-insertProducts("image18.JPG", "36", "Yellow Bracelet", "1", "medium", "bracelets", "");
-insertProducts("image19.JPG", "37", "Pink Earrings w/ Bracelet", "1", "medium", "earrings", "");
-insertProducts("image20.JPG", "38", "Colorful Earrings", "1", "medium", "earrings", "");
+insertProducts("image2.JPG", "20", "Pair of Pink Earrings", "1", "medium", "earrings", "Description of product here...");
+insertProducts("image3.jpg", "21", "Pair of Pink Bracelets", "1", "medium", "bracelets", "Description of product here...");
+insertProducts("image6.JPG", "22", "Pin", "1", "medium", "scarf", "Description of product here...");
+insertProducts("image1.jpg", "23", "Yellow Necklace", "1", "medium", "necklaces", "Description of product here...");
+insertProducts("image4.jpg", "24", "Turquoise Bracelet with Pattern", "1", "medium", "bracelets", "Description of product here...");
+insertProducts("image9.JPG", "25", "Multiple Colorful Pins", "1", "medium", "scarf", "Description of product here...");
+insertProducts("image7.jpg", "26", "Two Hexagon-Shaped Pins", "1", "medium", "scarf", "Description of product here...");
+insertProducts("image8.JPG", "27", "Red and Colorful Pins", "1", "medium", "scarf", "Description of product here...");
+insertProducts("image10.JPG", "28", "Yellow Earrings", "1", "medium", "earrings", "Description of product here...");
+insertProducts("image11.JPG", "29", "Pink Earrings", "1", "medium", "earrings", "Description of product here...");
+insertProducts("image12.JPG", "30", "Orange Earrings w/ Bracelet", "1", "medium", "earrings", "Description of product here...");
+insertProducts("image13.JPG", "31", "Blue Earrings w/ Necklace", "1", "medium", "earrings", "Description of product here...");
+insertProducts("image14.JPG", "32", "Red Earrings w/ Necklace", "1", "medium", "earrings", "Description of product here...");
+insertProducts("image15.JPG", "33", "Blue and Pink Bracelet", "1", "medium", "bracelets", "Description of product here...");
+insertProducts("image16.JPG", "34", "Turquoise Bracelet", "1", "medium", "bracelets", "Description of product here...");
+insertProducts("image17.JPG", "35", "Red and White Pin", "1", "medium", "scarf", "Description of product here...");
+insertProducts("image18.JPG", "36", "Yellow Bracelet", "1", "medium", "bracelets", "Description of product here...");
+insertProducts("image19.JPG", "37", "Pink Earrings w/ Bracelet", "1", "medium", "earrings", "Description of product here...");
+insertProducts("image20.JPG", "38", "Colorful Earrings", "1", "medium", "earrings", "Description of product here...");
 
 
 
